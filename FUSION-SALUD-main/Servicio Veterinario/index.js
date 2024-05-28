f61fdceb966305689bcf1c7536cd1a76a28a2752
meloneitor <josegab1121@gmail.com>
2024-05-28 16:11:05 -0500

    Se cambia error en el slider

--- FUSION-SALUD-main/Servicio Veterinario/index.js
+++ FUSION-SALUD-main/Servicio Veterinario/index.js
@@ -16,14 +16,19 @@ setInterval(function(){
 
 
 function slides(){
-    slider.style.transform = "translate("+(-width*contador)+"px)";
-    slider.style.transition = "transform 2s";
+    slider.style = `
+      transform : translate(-${width*contador}px);
+      transition : transform 2s;
+      z-index : -1;
+      position : relative;
+    `
     contador++;
 
     if(contador == sliderIndividual.length){
         setTimeout(function(){
             slider.style.transform = "translate(0px)";
             slider.style.transition = "transform 1s";
+            
             contador=1;
         },1500)
     }
